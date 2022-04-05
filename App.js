07869f5bcb06e6a5610fa298a2911d020f7033e7
Steven Sidhu <steve.sidhu@hotmail.com>
2022-04-05 02:17:17 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+const SpeakifyScreen = ({ navigation }) => {
+  const webviewRef = React.useRef(null);
+  const onMessage = (data) => {
+    alert(data.nativeEvent.data);
+    console.log(data.nativeEvent.data);
+    navigation.navigate("Budget");
+  };
+  return (
+    <WebView
+      source={{ uri: "https://tensor-testing-native.herokuapp.com/" }}
+      style={{ marginTop: -10, marginRight: 0, height: 200 }}
+      // renderLoading={LoadingIndicatorView}
+      startInLoadingState={true}
+      scrollEnabled={false}
+      bounces={false}
+      ref={webviewRef}
+      onMessage={onMessage}
+    />
+  );
+};
+
+export default SpeakifyScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#141117",
+  },
+});
